1315aa1dac22a2d29b90297fb2b48b806c35dfc1
obsidian-git-auto-commit <111306950+Firehawk4427@users.noreply.github.com>
2024-08-05 12:51:14 -0700

    vault backup: 2024-08-05 12:51:14

--- pillars_v2_airbag_open/lsruncommand.bat
+++ pillars_v2_airbag_open/lsruncommand.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\LS-DYNA Suite R13 Student\lsdyna\ls-dyna_smp_d_R13.1.1_27-g8731a0d8c5_winx64_ifort190.exe" i=C:\Users\Trey\Documents\GitHub\PILLARS-LSDyna\pillars_v2_airbag_open\open_airbag_v63.k ncpu=2 memory=20m > lsrun.out.txt 2>&1
+"C:\Program Files\LS-DYNA Suite R13 Student\lsdyna\ls-dyna_smp_d_R13.1.1_27-g8731a0d8c5_winx64_ifort190.exe" i=C:\Users\Trey\Documents\GitHub\PILLARS-LSDyna\pillars_v2_airbag_open\open_airbag_v67.k ncpu=2 memory=20m > lsrun.out.txt 2>&1
